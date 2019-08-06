610a90c47d015e022eadf1255a061feab1b6c22f
Duc Nguyen <duc.nguyenthanh1995@@gmail.com>
2019-08-06 16:44:56 +0700

    Add DDWrapperView

--- DDTabBarItem.swift
+++ DDTabBarItem.swift
@@ -7,46 +7,46 @@
 
 import Foundation
 
+public enum DDWrapperMode {
+    case normal
+    case rounded
+}
+
 open class DDTabBarItem: UIButton {
-    open private(set) var ddBackgroundImage: UIImage?
     private var icon: UIImage?
     private var title: String?
     private var iconHeight: CGFloat = DDConstant.iconHeight
     open var iconImageView: UIImageView?
     open var ddTitleLabel: UILabel?
-    open var ddBackgroundImageView: UIImageView?
-    
+    open var ddContentWrapperView: UIView?
     
     var height: CGFloat = -1
     
-    public convenience init(height: CGFloat = -1, icon: UIImage, title: String?, iconHeight: CGFloat = DDConstant.iconHeight, backgroundImage: UIImage? = nil) {
+    public convenience init(height: CGFloat = -1, icon: UIImage, title: String?, iconHeight: CGFloat = DDConstant.iconHeight, backgroundImage: UIImage? = nil, wrapperMode: DDWrapperMode = .normal) {
         self.init()
         self.height = height
-        setupDDBackgroundImageView(withImage: backgroundImage)
+        setupDDContentWrapperView(wrapperMode: wrapperMode)
         setupIcon(withImage: icon, iconHeight: iconHeight)
         setupTitle(withTitle: title)
     }
     
-    func setupDDBackgroundImageView(withImage image: UIImage?) {
-        let imageView = UIImageView()
-        addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.contentMode = .scaleAspectFit
-        imageView.image = image
+    func setupDDContentWrapperView(wrapperMode: DDWrapperMode = .normal) {
+        let wrapperView = DDWrapperView(wrapperMode: wrapperMode)
+        addSubview(wrapperView)
+        wrapperView.translatesAutoresizingMaskIntoConstraints = false
+        wrapperView.isUserInteractionEnabled = false
         
         // Constraints
-        imageView.widthHeightRatio(ratio: 1)
-        imageView.equalWidth(toView: self, multiplier: 1)
-        imageView.equalHeight(toView: self, multiplier: 1, relatedBy: .lessThanOrEqual)
-        imageView.centerX(toView: self)
-        imageView.top(toView: self)
+        wrapperView.top(toView: self)
+        wrapperView.widthHeightRatio(ratio: 1)
+        wrapperView.centerX(toView: self)
 
-        ddBackgroundImageView = imageView
+        ddContentWrapperView = wrapperView
     }
     
     func setupIcon(withImage image: UIImage, iconHeight: CGFloat) {
         let imageView = UIImageView()
-        addSubview(imageView)
+        ddContentWrapperView?.addSubview(imageView)
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
         imageView.image = image
@@ -54,18 +54,20 @@ open class DDTabBarItem: UIButton {
         
         // Constraints
         imageView.widthHeightRatio(ratio: 1)
-        imageView.equalWidth(toView: self, multiplier: 1, relatedBy: .lessThanOrEqual)
-        imageView.equalHeight(toView: self, multiplier: 1, relatedBy: .lessThanOrEqual)
+        imageView.equalWidth(toView: self, constant: -10, relatedBy: .lessThanOrEqual)
+        imageView.equalHeight(toView: self, relatedBy: .lessThanOrEqual)
         imageView.centerX(toView: self)
-        imageView.top(toView: self, constant: 10)
         imageView.height(iconHeight, priority: .init(999))
+        imageView.top(toView: ddContentWrapperView, constant: 10)
+        imageView.leading(toView: ddContentWrapperView, relatedBy: .greaterThanOrEqual)
+        imageView.trailing(toView: ddContentWrapperView, relatedBy: .lessThanOrEqual)
         
         iconImageView = imageView
     }
     
     func setupTitle(withTitle title: String?) {
         let label = UILabel()
-        addSubview(label)
+        ddContentWrapperView?.addSubview(label)
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = title
         label.textColor = DDConstant.barItemColor
@@ -73,10 +75,12 @@ open class DDTabBarItem: UIButton {
         label.font = DDConstant.barItemTitleFont
         
         // Constraints
-        label.equalWidth(toView: self, multiplier: 1, relatedBy: .lessThanOrEqual)
+        label.equalWidth(toView: self, constant: -10, relatedBy: .lessThanOrEqual)
         label.topSpacing(toView: iconImageView, constant: 0)
         label.centerX(toView: self)
-        label.bottom(toView: self, constant: 10,relatedBy: .lessThanOrEqual)
+        label.bottom(toView: ddContentWrapperView, constant: -10, relatedBy: .lessThanOrEqual)
+        label.leading(toView: ddContentWrapperView, relatedBy: .greaterThanOrEqual)
+        label.trailing(toView: ddContentWrapperView, relatedBy: .lessThanOrEqual)
         ddTitleLabel = label
     }
     
@@ -91,12 +95,33 @@ open class DDTabBarItem: UIButton {
     }
     
     open func setDDBackgroundImage(_ image: UIImage?) {
-        ddBackgroundImage = image
-        ddBackgroundImageView?.image = image
     }
     
     func setColor(_ color: UIColor) {
         tintColor = color
         ddTitleLabel?.textColor = color
+    }
+}
+
+class DDWrapperView: UIView {
+    open var wrapperMode: DDWrapperMode = .normal
+    
+    convenience init(wrapperMode: DDWrapperMode) {
+        self.init()
+        self.wrapperMode = wrapperMode
+        setupView()
+    }
+    
+    func setupView() {}
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        switch wrapperMode {
+        case .normal:
+            break
+        case .rounded:
+            layer.cornerRadius = frame.width/2
+            break
+        }
     }
 }
--- UIView+Extensions.swift
+++ UIView+Extensions.swift
@@ -27,57 +27,61 @@ extension UIView {
         addConstraint(constraint)
     }
     
-    func equalWidth(toView view: UIView?, multiplier: CGFloat, relatedBy relation: NSLayoutConstraint.Relation = .equal, priority: UILayoutPriority = .required) {
-        let constraint = NSLayoutConstraint(item: self, attribute: .width, relatedBy: relation, toItem: view, attribute: .width, multiplier: multiplier, constant: 0)
+    func equalWidth(toView view: UIView?, constant: CGFloat = 0, multiplier: CGFloat = 1, relatedBy relation: NSLayoutConstraint.Relation = .equal, priority: UILayoutPriority = .required) {
+        let constraint = NSLayoutConstraint(item: self, attribute: .width, relatedBy: relation, toItem: view, attribute: .width, multiplier: multiplier, constant: constant)
         constraint.priority = priority
-        (superview ?? view)?.addConstraint(constraint)
+        (highestSuperview() ?? view)?.addConstraint(constraint)
     }
     
-    func equalHeight(toView view: UIView?, multiplier: CGFloat, relatedBy relation: NSLayoutConstraint.Relation = .equal, priority: UILayoutPriority = .required) {
-        let constraint = NSLayoutConstraint(item: self, attribute: .height, relatedBy: relation, toItem: view, attribute: .height, multiplier: multiplier, constant: 0)
+    func equalHeight(toView view: UIView?, constant: CGFloat = 0, multiplier: CGFloat = 1, relatedBy relation: NSLayoutConstraint.Relation = .equal, priority: UILayoutPriority = .required) {
+        let constraint = NSLayoutConstraint(item: self, attribute: .height, relatedBy: relation, toItem: view, attribute: .height, multiplier: multiplier, constant: constant)
         constraint.priority = priority
-        (superview ?? view)?.addConstraint(constraint)
+        (highestSuperview() ?? view)?.addConstraint(constraint)
     }
     
     func leading(toView view: UIView?, constant: CGFloat = 0, relatedBy relation: NSLayoutConstraint.Relation = .equal, priority: UILayoutPriority = .required) {
         let constraint = NSLayoutConstraint(item: self, attribute: .leading, relatedBy: relation, toItem: view, attribute: .leading, multiplier: 1, constant: constant)
         constraint.priority = priority
-        (superview ?? view)?.addConstraint(constraint)
+        (highestSuperview() ?? view)?.addConstraint(constraint)
     }
     
     func trailing(toView view: UIView?, constant: CGFloat = 0, relatedBy relation: NSLayoutConstraint.Relation = .equal, priority: UILayoutPriority = .required) {
         let constraint = NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: relation, toItem: view, attribute: .trailing, multiplier: 1, constant: constant)
         constraint.priority = priority
-        (superview ?? view)?.addConstraint(constraint)
+        (highestSuperview() ?? view)?.addConstraint(constraint)
     }
     
     func top(toView view: UIView?, constant: CGFloat = 0, relatedBy relation: NSLayoutConstraint.Relation = .equal, priority: UILayoutPriority = .required) {
         let constraint = NSLayoutConstraint(item: self, attribute: .top, relatedBy: relation, toItem: view, attribute: .top, multiplier: 1, constant: constant)
         constraint.priority = priority
-        (superview ?? view)?.addConstraint(constraint)
+        (highestSuperview() ?? view)?.addConstraint(constraint)
     }
     
     func topSpacing(toView view: UIView?, constant: CGFloat = 0, relatedBy relation: NSLayoutConstraint.Relation = .equal, priority: UILayoutPriority = .required) {
         let constraint = NSLayoutConstraint(item: self, attribute: .top, relatedBy: relation, toItem: view, attribute: .bottom, multiplier: 1, constant: constant)
         constraint.priority = priority
-        (superview ?? view)?.addConstraint(constraint)
+        (highestSuperview() ?? view)?.addConstraint(constraint)
     }
 
     func bottom(toView view: UIView?, constant: CGFloat = 0, relatedBy relation: NSLayoutConstraint.Relation = .equal, priority: UILayoutPriority = .required) {
         let constraint = NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: relation, toItem: view, attribute: .bottom, multiplier: 1, constant: constant)
         constraint.priority = priority
-        (superview ?? view)?.addConstraint(constraint)
+        (highestSuperview() ?? view)?.addConstraint(constraint)
     }
     
     func centerX(toView view: UIView?, priority: UILayoutPriority = .required) {
         let constraint = NSLayoutConstraint(item: self, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0)
         constraint.priority = priority
-        (superview ?? view)?.addConstraint(constraint)
+        (highestSuperview() ?? view)?.addConstraint(constraint)
     }
     
     func centerY(toView view: UIView?, priority: UILayoutPriority = .required) {
         let constraint = NSLayoutConstraint(item: self, attribute: .centerY, relatedBy: .equal, toItem: view, attribute: .centerY, multiplier: 1, constant: 0)
         constraint.priority = priority
-        (superview ?? view)?.addConstraint(constraint)
+        (highestSuperview() ?? view)?.addConstraint(constraint)
+    }
+    
+    func highestSuperview() -> UIView? {
+        return superview?.highestSuperview() ?? superview
     }
 }
